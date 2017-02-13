0e7dde9a4309017648daac6752030e17590692c7
chrisminnick <chris@watzthis.com>
2017-02-13 11:13:12 -0800

    console lab

---
+++ solutions/lab15.5/script.js
@@ -0,0 +1,8 @@
+var somethingiswrong = 'nothing is wrong!';
+console.log('This is a console log message.');
+console.log('When errors happen in your program, this is where JavaScript will tell you about them. Like this: ');
+console.log(somethingiswrong);
+console.log('You can also use the console.log method in your programs to help you debug or test things');
+console.log('For example, you might output the value of a variable or the result of an expression here.');
+console.log('If I have ten apples and someone gives me 4 more, how many apples do I have?');
+console.log('The answer is ' + 10+4);
