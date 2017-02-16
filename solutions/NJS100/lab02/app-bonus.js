b83b88373ae9ded54a5cd58689d416d9f6d2ba22
chrisminnick <chris@watzthis.com>
2017-02-16 08:11:08 -0800

    reorganized and new node labs

--- solutions/NJS100/lab02/app-bonus.js
+++ solutions/NJS100/lab02/app-bonus.js
@@ -1,11 +1,17 @@
 var http = require('http');
 var urlToGet = process.argv[2];
 
+var output;
 http.get(urlToGet, function(response) {
 
     response.on("data", function(chunk) {
-        console.log("BODY: " + chunk.toString());
+        output += chunk.toString();
     });
+    response.on("end", function(){
+        console.log(output);
+        console.log("output length: " + output.length);
+    });
+
 }).on('error', function(e) {
     console.log("Got error: " + e.message);
 });
